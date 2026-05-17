376bf005fbd9c170bfb32b5fefd0cfff4074b15f
Avinash0397 <avimishra478@gmail.com>
2026-05-17 19:35:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ data "aws_subnets" "default_subnets" {
 # IAM ROLE FOR EKS CLUSTER
 # -------------------------
 
-resource "aws_iam_role" "eks_role1" {
-  name = "my-eks-terraform1"
+resource "aws_iam_role" "eks_role2" {
+  name = "my-eks-terraform2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -71,7 +71,7 @@ resource "aws_iam_role" "eks_role1" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_role1.name
+  role       = aws_iam_role.eks_role2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role1.arn
+  role_arn = aws_iam_role.eks_role2.arn
 
   vpc_config {
     subnet_ids              = data.aws_subnets.default_subnets.ids
@@ -97,8 +97,8 @@ resource "aws_eks_cluster" "eks_cluster" {
 # IAM ROLE FOR NODE GROUP
 # -------------------------
 
-resource "aws_iam_role" "eks_node_role1" {
-  name = "my-eks-node-role-terraform1"
+resource "aws_iam_role" "eks_node_role2" {
+  name = "my-eks-node-role-terraform2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -113,17 +113,17 @@ resource "aws_iam_role" "eks_node_role1" {
 }
 
 resource "aws_iam_role_policy_attachment" "node_worker_policy" {
-  role       = aws_iam_role.eks_node_role1.name
+  role       = aws_iam_role.eks_node_role2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "node_cni_policy" {
-  role       = aws_iam_role.eks_node_role1.name
+  role       = aws_iam_role.eks_node_role2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
-  role       = aws_iam_role.eks_node_role1.name
+  role       = aws_iam_role.eks_node_role2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
@@ -134,7 +134,7 @@ resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role1.arn
+  node_role_arn   = aws_iam_role.eks_node_role2.arn
   subnet_ids      = data.aws_subnets.default_subnets.ids
 
   # ⚠️ IMPORTANT FIX (recommended
